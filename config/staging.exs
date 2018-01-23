b84b1702c4bca28a6240a7d3511f8d84d1d7a8d2
cavewoman <anna.chan365@gmail.com>
2018-01-23 08:12:15 -0500

    Add staging config

---
+++ config/staging.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+ config :razor_test, RazorTest.Endpoint,
+   http: [port: {:system, "PORT"}],
+   url: [scheme: "https", host: {:system, "HOST"}, port: 443],
+   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+   cache_static_manifest: "priv/static/cache_manifest.json",
+   secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+ # Configure your database
+ config :razor_test, RazorTest.Repo,
+   adapter: Ecto.Adapters.Postgres,
+   url: System.get_env("DATABASE_URL"),
+   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+   ssl: true
