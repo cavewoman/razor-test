b26a4b7bff3076d92d3423e294209a05e8b91a50
cavewoman <anna.chan365@gmail.com>
2018-02-04 22:17:40 -0500

    hot-fix for missing colors

--- lib/razor_test_web/views/card_view.ex
+++ lib/razor_test_web/views/card_view.ex
@@ -7,41 +7,45 @@ defmodule RazorTestWeb.CardView do
 
   def getRowColor(colors, name) do
     card_color = ""
-    if (Enum.count(colors) == 1) do
-      case List.first(colors) do
-        "G" ->
-          card_color = "green-card"
-        "B" ->
-          IO.puts("BLACK")
-          card_color = "black-card"
-        "R" ->
-          card_color = "red-card"
-        "U" ->
-          card_color = "blue-card"
-        "W" ->
-          card_color = "white-card"
-        _ ->
-          card_color = "unknown-color-card"
+    if (colors) do
+      if (Enum.count(colors) == 1) do
+        case List.first(colors) do
+          "G" ->
+            card_color = "green-card"
+          "B" ->
+            IO.puts("BLACK")
+            card_color = "black-card"
+          "R" ->
+            card_color = "red-card"
+          "U" ->
+            card_color = "blue-card"
+          "W" ->
+            card_color = "white-card"
+          _ ->
+            card_color = "unknown-color-card"
+        end
       end
-    end
-    if (Enum.count(colors) > 1) do
-      card_color = "multicolor-card"
-    end
-    if (Enum.count(colors) == 0) do
-      case String.downcase(name) do
-        "forest" ->
-          card_color = "green-card"
-        "swamp" ->
-          card_color = "black-card"
-        "mountain" ->
-          card_color = "red-card"
-        "island" ->
-          card_color = "blue-card"
-        "plains" ->
-          card_color = "white-card"
-        _ ->
-          card_color = "unknown-color-card"
+      if (Enum.count(colors) > 1) do
+        card_color = "multicolor-card"
       end
+      if (Enum.count(colors) == 0) do
+        case String.downcase(name) do
+          "forest" ->
+            card_color = "green-card"
+          "swamp" ->
+            card_color = "black-card"
+          "mountain" ->
+            card_color = "red-card"
+          "island" ->
+            card_color = "blue-card"
+          "plains" ->
+            card_color = "white-card"
+          _ ->
+            card_color = "unknown-color-card"
+        end
+      end
+    else
+      card_color = "unknown-color-card"
     end
     card_color
   end
